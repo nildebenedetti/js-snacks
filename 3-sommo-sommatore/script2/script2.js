d49239207e6a1d32f86665743cdf73ca6ab69c73
“Nilde <nilde.benedetti@gmail.com>
2026-03-31 22:55:47 +0200

    creato array in index2.html

--- 3-sommo-sommatore/script2/script2.js
+++ 3-sommo-sommatore/script2/script2.js
@@ -3,3 +3,7 @@
 // BONUS: fai generare l'array all'utente tramite prompt. 
 // nel momento in cui l'utente scrive "SOMMA" restituisci 
 // in console o tramite alert la somma di tutti i valori inseriti dall'utente.
+
+const bolliniCoop = [5, 8, 9, 2, 5]; // creo array con bollini sparsi per le tasche delle giacche
+
+
