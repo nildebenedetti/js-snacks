4b95261954c0494874b931dbcba1f80654b464c6
“Nilde <nilde.benedetti@gmail.com>
2026-03-31 23:30:49 +0200

    aggiungo altri due input utente con prompt e rendo tutti i prompt Number per procedere poi con la somma per mezzo del ciclo for - script2.js

--- 3-sommo-sommatore/script2/script2.js
+++ 3-sommo-sommatore/script2/script2.js
@@ -8,21 +8,27 @@
 
 // BONUS
 const bolliniCoop =[]; // dichiaro array vuoto per pusharci risposte dell' User
+let contoBollini = 0; // dichiaro variabile che conterra' somma bollini
 
 // ora tocca a user frugare
 
-const primaFrugata = prompt('Dai una bella ravanata nella tasca della giacca. Quanti bollini Coop hai trovato?','7');
+const primaFrugata = Number(prompt('Dai una bella ravanata nella tasca della giacca. Quanti bollini Coop hai trovato?','7'));
 bolliniCoop.push(primaFrugata);
 
 console.log(`Lo sapevo, ecco ${primaFrugata} bollini.`)
 
-const secondaFrugata = prompt('Ora vedi un po nella borsa. Quanti bollini Coop hai trovato?','3');
+const secondaFrugata = Number(prompt('Ora vedi un po nella borsa. Quanti bollini Coop hai trovato?','3'));
 bolliniCoop.push(secondaFrugata);
 console.log(`Un classico, ecco ${secondaFrugata} bollini.`)
 
+const terzaFrugata = Number(prompt('Ora vediamo cosa trovi nei jeans di tuo fratello. Quanti bollini Coop hai trovato?','2'));
+bolliniCoop.push(terzaFrugata);
+console.log(`Pure lui li chiede quindi... ecco ${terzaFrugata} bollini.`)
 
+const quartaFrugata = Number(prompt('Per sicurezza, controlla la cuccia del cane. Quanti bollini Coop hai trovato?','8'));
+bolliniCoop.push(quartaFrugata);
+console.log(`Ma in che senso???? Ecco ${quartaFrugata} bollini. O___O`)
 
-let contoBollini = 0; // dichiaro variabile che conterra' somma bollini
 
 
 // creo un ciclo for che ad ogni giro somma ad ogni giro i bolllini ritrovati
@@ -32,4 +38,3 @@ for (let i = 0; i < bolliniCoop.length; i++) {
     contoBollini = contoBollini + currentCounter;
 }
 console.log(`Pazzesco! Frugando nelle tasche della tua famiglia, hai raccattato ${contoBollini} della Coop!`);
-
