3077ac1711ee89f785b809a66374d9fa45e84117
“Nilde <nilde.benedetti@gmail.com>
2026-03-31 22:51:59 +0200

    aggiornato nome folder consegna 3

---
+++ 3-sommo-sommatore/script2/script2.js
@@ -0,0 +1,5 @@
+// 3 - Dato un array di numeri, scrivi il codice 
+// che effettua la somma di tutti i valori. 
+// BONUS: fai generare l'array all'utente tramite prompt. 
+// nel momento in cui l'utente scrive "SOMMA" restituisci 
+// in console o tramite alert la somma di tutti i valori inseriti dall'utente.
