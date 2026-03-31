7261db3b21a07d53d2f6860fb629aba206250842
“Nilde <nilde.benedetti@gmail.com>
2026-03-31 23:09:05 +0200

    aggiunta variabile per conteggio e currentCounter per registrare numero bollini ad ogni giro

--- 3-sommo-sommatore/script2/script2.js
+++ 3-sommo-sommatore/script2/script2.js
@@ -4,6 +4,15 @@
 // nel momento in cui l'utente scrive "SOMMA" restituisci 
 // in console o tramite alert la somma di tutti i valori inseriti dall'utente.
 
-const bolliniCoop = [5, 8, 9, 2, 5]; // creo array con bollini sparsi per le tasche delle giacche
+const bolliniCoop = [5, 8, 9, 2, 5, 10]; // creo array con bollini sparsi per le tasche delle giacche
+let contoBollini = 0; // dichiaro variabile che conterra' somma bollini
 
+
+// creo un ciclo for che ad ogni giro somma ad ogni giro i bolllini ritrovati
+
+for (let i = 0; i < bolliniCoop.length; i++) {
+    currentCounter = bolliniCoop[i];
+    contoBollini = contoBollini + currentCounter;
+}
+console.log(`Pazzesco! Frugando nelle tasche della tua famiglia, hai raccattato ${contoBollini} della Coop!`);
 
