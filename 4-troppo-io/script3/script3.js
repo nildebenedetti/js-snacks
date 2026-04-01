391bd9427c6a37f00f46e0e1bee16a47265ab6f4
“Nilde <nilde.benedetti@gmail.com>
2026-04-01 17:54:15 +0200

    add: array vuoto per annotare stringhe comuni

--- 4-troppo-io/script3/script3.js
+++ 4-troppo-io/script3/script3.js
@@ -1,16 +1,21 @@
 // Dati due array, realizza uno script che mostri 
 // in console gli elementi comuni ai due array.
 
+// dichiaro i due array da confrontare
 const amicaUno = ['simpatica', 'brava a cucinare', 'sa cantare', 'crede negli oroscopi', 'usa il Dyson per i capelli', 'parla il tedesco'];
 const amicaDue = ['permalosa', 'ama il cioccolato', 'crede negli oroscopi', 'va in palestra', 'sa cantare'];
 
+
+
 function friendMatcher (firstFriend, secondFriend) {
+    // dichiaro array vuoto per pushare feature in comune
+    const commonFeatures = [];
     // ciclo for per far scorrere il primo array
     for (let i = 0; i < firstFriend.length; i++) {
         // variabile di supporto per identificare la feature del giro
         const currentFeature = firstFriend[i];
         if (secondFriend.contains(currentFeature) === true) {
-            
+
         }
 
     }
