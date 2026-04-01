01c92dd582098959372135af2d4be988a21c620f
“Nilde <nilde.benedetti@gmail.com>
2026-04-01 17:50:04 +0200

    add: 2 array per confronto elementi comuni; ciuclo for per scorrere primo array; variabile di supporto; in 4/script3.js

---
+++ 4-troppo-io/script3/script3.js
@@ -0,0 +1,14 @@
+// Dati due array, realizza uno script che mostri 
+// in console gli elementi comuni ai due array.
+
+const amicaUno = ['simpatica', 'brava a cucinare', 'sa cantare', 'crede negli oroscopi', 'usa il Dyson per i capelli', 'parla il tedesco'];
+const amicaDue = ['permalosa', 'ama il cioccolato', 'crede negli oroscopi', 'va in palestra', 'sa cantare'];
+
+function friendMatcher (firstFriend, secondFriend) {
+    // ciclo for per far scorrere il primo array
+    for (let i = 0; i < firstFriend.length; i++) {
+        // variabile di supporto per identificare la feature del giro
+        const currentFeatures = firstFriend[i];
+
+    }
+}
